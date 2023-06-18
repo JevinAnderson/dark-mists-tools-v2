7efdd5749cc6ac71dd8336a42e93c4a58fd8e34d
Jevin Anderson <jevinanderson@gmail.com>
2023-06-18 15:36:03 -0700

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { connect } from "react-redux";
 
-function Navigation() {
+import * as StyleActions from "../actions/styles";
+
+function Navigation({ darkMode, setDarkMode }) {
+  useEffect(() => {
+    if (darkMode) {
+      document.body.setAttribute("data-bs-theme", "dark");
+    } else {
+      document.body.setAttribute("data-bs-theme", "light");
+    }
+  }, [darkMode]);
+
   return (
     <>
       <Navbar
@@ -21,6 +33,14 @@ function Navigation() {
               <Nav.Link href="/">Items</Nav.Link>
               <Nav.Link href="/enchanters">Enchanters</Nav.Link>
               <Nav.Link href="#">Sign In</Nav.Link>
+              <NavDropdown title="Theme" id="basic-nav-dropdown">
+                <NavDropdown.Item onClick={() => setDarkMode(false)}>
+                  Light Mode
+                </NavDropdown.Item>
+                <NavDropdown.Item onClick={() => setDarkMode(true)}>
+                  Dark Mode
+                </NavDropdown.Item>
+              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -29,4 +49,9 @@ function Navigation() {
   );
 }
 
-export default Navigation;
+export default connect(
+  ({ styles = {} }) => ({
+    darkMode: styles.darkMode ?? false,
+  }),
+  StyleActions
+)(Navigation);
