aa2dab044cf62035aa87fbe313e96fb3ba75c3be
Jevin Anderson <jevinanderson@gmail.com>
2024-11-22 20:57:16 -0800

    Added footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
